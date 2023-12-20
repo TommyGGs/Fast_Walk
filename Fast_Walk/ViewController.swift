b18a5652a8c4727f630614fe6e26d9c9f1c6c199
TommyGGs <chentom397@gmail.com>
2023-12-20 19:07:30 +0900

    Time Selection, New Screen(RouteViewController)

--- Fast_Walk/ViewController.swift
+++ Fast_Walk/ViewController.swift
@@ -3,54 +3,75 @@ import GoogleMaps
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    
+
     @IBOutlet weak var mapContainerView: UIView!
     var locationManager = CLLocationManager()
     var mapView: GMSMapView?
     var currentLocation: CLLocationCoordinate2D?
-    
+    var currentRoutePolyline: GMSPolyline?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //force update location status and if so, begin updating location information.
         locationManager.delegate = self
         beginLocationUpdate()
         
         let camera = GMSCameraPosition.camera(withLatitude: 0, longitude: 0, zoom: 10.0)
-        mapView = GMSMapView.init(frame: mapContainerView.bounds, camera: camera)
+        mapView = GMSMapView.map(withFrame: mapContainerView.bounds, camera: camera)
         mapView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         mapContainerView.addSubview(mapView!)
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         beginLocationUpdate()
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first, currentLocation == nil {
             currentLocation = location.coordinate
-            
-            if let mapView = mapView {
-                mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 10.0)
-                mapView.isMyLocationEnabled = true
-                mapView.settings.myLocationButton = true
-            }
-            
-            //locationManager.stopUpdatingLocation()
-            findBestRoute(from: location.coordinate)
+            mapView?.camera = GMSCameraPosition(target: location.coordinate, zoom: 10.0)
+            mapView?.isMyLocationEnabled = true
+            mapView?.settings.myLocationButton = true
+            locationManager.stopUpdatingLocation()
         }
     }
-    func findBestRoute(from start: CLLocationCoordinate2D) {
+
+    func calculateWaypointAdjustment(for desiredTime: Int) -> Double {
+        // Example logic to adjust waypoint range based on desiredTime
+        // This needs to be tuned according to your specific use case
+        switch desiredTime {
+        case 30:
+            return 0.005 // Smaller range for shorter routes
+        case 45:
+            return 0.010 // Medium range
+        case 60, 90:
+            return 0.015 // Larger range for longer routes
+        default:
+            return 0.005 // Default range
+        }
+    }
+    
+    @IBAction func searchRoute(_ sender: UIButton) {
+        currentRoutePolyline?.map = nil
+        if let currentLocation = self.currentLocation,
+           let buttonText = sender.titleLabel?.text,
+           let desiredTime = Int(buttonText.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+            findBestRoute(from: currentLocation, desiredTime: desiredTime)
+        } else {
+            print("Current location is not available or invalid route time.")
+        }
+    }
+
+    func findBestRoute(from start: CLLocationCoordinate2D, desiredTime: Int) {
         var closestDuration = Int.max
         var bestRouteDetails: (polyString: String, durationText: String)?
         let group = DispatchGroup()
         
         for _ in 1...2 {
             group.enter()
-            let waypoints = generateRandomWaypoints(from: start, count: 2)
+            let waypoints = generateRandomWaypoints(from: start, count: 2, adjustment: calculateWaypointAdjustment(for: desiredTime))
             requestRoute(from: start, waypoints: waypoints) { polyString, duration in
                 let durationInMinutes = duration / 60
-                if abs(durationInMinutes - 30) < abs(closestDuration - 30) {
+                if abs(durationInMinutes - desiredTime) < abs(closestDuration - desiredTime) {
                     closestDuration = durationInMinutes
                     bestRouteDetails = (polyString, "\(durationInMinutes) min")
                 }
@@ -64,13 +85,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             }
         }
     }
-    
-    func generateRandomWaypoints(from start: CLLocationCoordinate2D, count: Int) -> [CLLocationCoordinate2D] {
+
+    func generateRandomWaypoints(from start: CLLocationCoordinate2D, count: Int, adjustment: Double) -> [CLLocationCoordinate2D] {
         return (1...count).map { _ in
-            CLLocationCoordinate2D(
-                latitude: start.latitude + randomCoordinateOffset(),
-                longitude: start.longitude + randomCoordinateOffset()
-            )
+            CLLocationCoordinate2D(latitude: start.latitude + Double.random(in: -adjustment...adjustment),
+                                    longitude: start.longitude + Double.random(in: -adjustment...adjustment))
         }
     }
     
@@ -121,28 +140,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func displayRouteOnMap(polyString: String, start: CLLocationCoordinate2D, durationText: String) {
-        if let path = GMSPath(fromEncodedPath: polyString), let mapView = self.mapView {
+        if let path = GMSPath(fromEncodedPath: polyString) {
             let polyline = GMSPolyline(path: path)
             polyline.strokeWidth = 5.0
             polyline.strokeColor = UIColor.systemBlue
             polyline.map = mapView
             
+            self.currentRoutePolyline = polyline
+
             let bounds = GMSCoordinateBounds(path: path)
             let update = GMSCameraUpdate.fit(bounds, withPadding: 50)
-            mapView.animate(with: update)
+            mapView?.animate(with: update)
             
             let durationMarker = GMSMarker(position: start)
             durationMarker.title = "Route Start"
             durationMarker.snippet = "Estimated Total Walking Time: \(durationText)"
             durationMarker.map = mapView
-            mapView.selectedMarker = durationMarker
+            mapView?.selectedMarker = durationMarker
         } else {
             print("Failed to draw the route on the map.")
         }
     }
     
     func randomCoordinateOffset() -> Double {
-        return Double.random(in: -0.015...0.015)
+        return Double.random(in: -0.005...0.005)
     }
     
     func beginLocationUpdate(){
