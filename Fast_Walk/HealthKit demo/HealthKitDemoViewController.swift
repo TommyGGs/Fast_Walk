1c8767d8a85308e9f361bd2ac503dfef4320e62d
jiaxu0715 <104713022+jiaxu0715@users.noreply.github.com>
2024-01-05 20:00:15 +0900

    partially fixed files modified before tom commit.

--- Fast_Walk/HealthKit demo/HealthKitDemoViewController.swift
+++ Fast_Walk/HealthKit demo/HealthKitDemoViewController.swift
@@ -1,39 +1,36 @@
-//
-//  Healthdata.swift
-//  Fast_Walk
-//
-//  Created by visitor on 2024/01/02.
-//
-
-import Foundation
 import UIKit
 import HealthKit
+import CareKit
+import CareKitUI
 
 class HealthKitDemoViewController: UIViewController {
     
     @IBOutlet weak var stepsLabel: UILabel!
-    
-    
-    let store = HealthKitData.healthStore
-    
-    
+
+    let healthStore = HKHealthStore()
+    var anchor: HKQueryAnchor?
+    //var timer: Timer!
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         authorizeHealthKit()
-        
-        
     }
-    
-    @IBAction func press(_ sender: Any) {
-        
-        fetchStepData { steps in
-            DispatchQueue.main.async {
-                self.stepsLabel.text = "\(steps) steps"
-                self.updateStepsLabel(steps)
-            }
-        }
+
+    func startWorkoutSession() {
+        startObservingSteps()
+//        timer = Timer.scheduledTimer(timeInterval: 180, target: self, selector: #selector(updateStepCount), userInfo: nil, repeats: true)
     }
-    //Authorize HealthKit
+
+    func endWorkoutSession() {
+//        timer?.invalidate()
+//        timer = nil
+    }
+
+    @objc func updateStepCount() {
+        fetchStepData()
+    }
+
+    // Authorization
     func authorizeHealthKit() {
         guard HKHealthStore.isHealthDataAvailable() else {
             return
@@ -41,55 +38,43 @@ class HealthKitDemoViewController: UIViewController {
         
         let readTypes = Set([HKObjectType.quantityType(forIdentifier: .stepCount)!])
         
-        store.requestAuthorization(toShare: nil, read: readTypes) { (success, error) in
+        healthStore.requestAuthorization(toShare: nil, read: readTypes) { (success, error) in
             if !success {
                 // Handle errors here.
-                print("error requesting authorization")
+                print("Error requesting authorization: \(String(describing: error))")
             }
         }
     }
-    //fetchSteps for last 7 days, by day.
-    func fetchStepData(completion: @escaping ([Int]) -> Void) {
-        guard let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount) else {
-            completion([])
+
+    // Fetch Steps
+    func fetchStepData() {
+        guard let stepsType = HKObjectType.quantityType(forIdentifier: .stepCount) else {
             return
         }
 
-        let now = Date()
-        let startOfDay = Calendar.current.startOfDay(for: now)
-        let startDate = Calendar.current.date(byAdding: .day, value: -6, to: startOfDay) // last 7 days including today
-        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: now, options: .strictStartDate)
-
-        var dateComponents = DateComponents()
-        dateComponents.day = 1
-
-        let query = HKStatisticsCollectionQuery(quantityType: stepsQuantityType,
-                                                quantitySamplePredicate: predicate,
-                                                options: .cumulativeSum,
-                                                anchorDate: startDate!,
-                                                intervalComponents: dateComponents)
-
-        query.initialResultsHandler = { query, results, error in
-            guard let statsCollection = results else {
-                completion([])
-                return
+        let query = HKAnchoredObjectQuery(type: stepsType, predicate: nil, anchor: anchor, limit: HKObjectQueryNoLimit) { [weak self] query, sampleObjects, deletedObjects, newAnchor, error in
+            guard let self = self else { return }
+            
+            if let newAnchor = newAnchor {
+                self.anchor = newAnchor
             }
 
-            var dailySteps = [Int]()
-            let endDate = Calendar.current.startOfDay(for: now)
-            statsCollection.enumerateStatistics(from: startDate!, to: endDate) { statistics, stop in
-                let count = statistics.sumQuantity()?.doubleValue(for: HKUnit.count()) ?? 0
-                dailySteps.append(Int(count))
+            if let samples = sampleObjects as? [HKQuantitySample] {
+                let totalSteps = samples.map { $0.quantity.doubleValue(for: HKUnit.count()) }.reduce(0, +)
+                DispatchQueue.main.async {
+                    self.stepsLabel.text = "Steps: \(Int(totalSteps))"
+                }
             }
-
-            completion(dailySteps)
         }
 
-        store.execute(query)
+        healthStore.execute(query)
     }
     
-    private func updateStepsLabel(_ stepsArray: [Int]) {
-            let stepsString = stepsArray.map { String($0) }.joined(separator: ",")
-            stepsLabel.text = stepsString
-        }
-}
+
+    // Start Observing Steps
+    func startObservingSteps() {
+        fetchStepData()
+    }
+    
+  }
+
