515fb3d9bf7d79e0b0bf041683fbecac8d18afff
jiaxu0715 <jiaxu0715@gmail.com>
2024-01-26 23:34:02 +0900

    fixed routeview gradient, font.

--- Fast_Walk/HealthKit demo/HealthKitDemoViewController.swift
+++ Fast_Walk/HealthKit demo/HealthKitDemoViewController.swift
@@ -18,7 +18,7 @@ class HealthKitDemoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setGradientBackground()
+//        setGradientBackground()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -94,23 +94,23 @@ class HealthKitDemoViewController: UIViewController {
 //    }
     
     
-    func setGradientBackground() {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = view.bounds
-        
-        // Set your custom colors
-        gradientLayer.colors = [
-            UIColor(red: 0x45/255.0, green: 0xB1/255.0, blue: 0xFF/255.0, alpha: 1.0).cgColor,
-            UIColor(red: 0x00/255.0, green: 0x21/255.0, blue: 0xCD/255.0, alpha: 1.0).cgColor
-        ]
-
-        // You can customize the direction of the gradient if needed
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.8)
-        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
-
-        // Add the gradient layer to your view's layer
-        view.layer.insertSublayer(gradientLayer, at: 0)
-    }
+//    func setGradientBackground() {
+//        let gradientLayer = CAGradientLayer()
+//        gradientLayer.frame = view.bounds
+//        
+//        // Set your custom colors
+//        gradientLayer.colors = [
+//            UIColor(red: 0x45/255.0, green: 0xB1/255.0, blue: 0xFF/255.0, alpha: 1.0).cgColor,
+//            UIColor(red: 0x00/255.0, green: 0x21/255.0, blue: 0xCD/255.0, alpha: 1.0).cgColor
+//        ]
+//
+//        // You can customize the direction of the gradient if needed
+//        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.8)
+//        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
+//
+//        // Add the gradient layer to your view's layer
+//        view.layer.insertSublayer(gradientLayer, at: 0)
+//    }
     
     
     func startPedometerUpdates() {
